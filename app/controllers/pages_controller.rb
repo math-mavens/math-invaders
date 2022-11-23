15ae392307eec3357ba93598bd773e5d8f9bc3a4
Samdu Ngedup <samdungedup@gmail.com>
2022-11-23 22:53:26 +0800

    stats

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,7 @@ class PagesController < ApplicationController
   def dashboard
     @levels = Level.all
     @attempts = Attempt.where(user: current_user)
+    @all_time = @attempts.map(&:total_time).compact
+    @total_problems = @attempts.map(&:problems_solved).compact
   end
 end
---
+++ app/models/UserLevel.rb
@@ -0,0 +1,19 @@
+class UserLevels
+
+  def initialize(attempt, level, user)
+    @attempt = attempt
+    @level = level
+    @user = user
+  end
+
+  def user_attempt
+  end
+
+end
+
+
+# Level
+#   Attempts[](User, level, attempts[])
+
+#   getBestTime()
+#   getPlayedNumber()
