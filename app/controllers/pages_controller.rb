c6aa1721e319316c1933e68ef93cdab56b55e33f
Gautam Gupta <ggupta78@gmail.com>
2022-11-25 12:41:07 +0800

    implement level stats on game card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,14 @@ class PagesController < ApplicationController
 
   def dashboard
     @levels = Level.all
+    @user_levels = []
+
+    @levels.each do |level|
+      @level_attempts = Attempt.where(user: current_user, level: level)
+      @user_level = UserLevel.new(level, @level_attempts)
+      @user_levels << @user_level
+    end
+
     @attempts = Attempt.where(user: current_user)
     @all_time = @attempts.map(&:total_time).compact
     @total_problems = @attempts.map(&:problems_solved).compact
---
+++ app/models/user_level.rb
@@ -0,0 +1,29 @@
+class UserLevel
+  def initialize(level, attempts)
+    @attempts = attempts
+    @level = level
+  end
+
+  def level_name
+    @level.name
+  end
+
+  def category_name
+    @level.category.name
+  end
+
+  def level_id
+    @level.id
+  end
+
+  def best_time
+    return "NA" if @attempts.nil?
+
+    @all_time = @attempts.map(&:total_time).compact
+    "#{@all_time.min / 1000.0} secs"
+  end
+
+  def attempts_count
+    @attempts.count
+  end
+end
