0e67003d05bf52d0f8e5c99b174acebd86982a4e
Gautam Gupta <ggupta78@gmail.com>
2022-12-01 10:57:03 +0800

    small fixes

--- public/game/Levels.js
+++ public/game/Levels.js
@@ -1,8 +1,8 @@
 const Levels = {
   level1: [
-    [null, null, {name:'one',strength:2,trigger:false}, {name:'plus',strength:2,trigger:false}, {name:'two',strength:2,trigger:false}, {name:'plus',strength:2,trigger:false}, {name:'four',strength:1,trigger:true}, {name:'equal',strength:2,trigger:false}, {name:'three',strength:2,trigger:false}, null],
-    [null, null, {name:'one',strength:2,trigger:false}, {name:'plus',strength:2,trigger:false}, {name:'two',strength:2,trigger:false}, {name:'plus',strength:2,trigger:false}, {name:'four',strength:2,trigger:true}, {name:'equal',strength:2,trigger:false}, {name:'three',strength:2,trigger:false}, null],
-    [null, null, {name:'one',strength:2,trigger:false}, {name:'plus',strength:2,trigger:false}, {name:'two',strength:2,trigger:false}, {name:'plus',strength:2,trigger:false}, {name:'four',strength:2,trigger:true}, {name:'equal',strength:2,trigger:false}, {name:'three',strength:2,trigger:false}, null]
+    [{name:'one',strength:2,trigger:false}, {name:'plus',strength:2,trigger:false}, {name:'two',strength:2,trigger:false}, {name:'plus',strength:2,trigger:false}, {name:'four',strength:1,trigger:true}, {name:'equal',strength:2,trigger:false}, {name:'three',strength:2,trigger:false}, null, null, null],
+    [null, {name:'one',strength:2,trigger:false}, {name:'plus',strength:2,trigger:false}, {name:'two',strength:2,trigger:false}, {name:'plus',strength:2,trigger:false}, {name:'four',strength:2,trigger:true}, {name:'equal',strength:2,trigger:false}, {name:'three',strength:2,trigger:false}, null, null],
+    [{name:'one',strength:2,trigger:false}, {name:'plus',strength:2,trigger:false}, {name:'two',strength:2,trigger:false}, {name:'plus',strength:2,trigger:false}, {name:'four',strength:2,trigger:true}, {name:'equal',strength:2,trigger:false}, {name:'three',strength:2,trigger:false}, null, null, null]
   ],
   level2: [
     [null, {name:'one',strength:2,trigger:false}, {name:'plus',strength:2,trigger:false}, {name:'two',strength:2,trigger:false}, {name:'plus',strength:2,trigger:false}, {name:'four',strength:1,trigger:true}, {name:'equal',strength:2,trigger:false}, {name:'three',strength:2,trigger:false}, null, null],
